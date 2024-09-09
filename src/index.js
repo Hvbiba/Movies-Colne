1dd7ba83e28f3eb429856937b8bc26c343c3c07d
habiba <ashrafhabiba002@gmail.com>
2024-09-09 20:22:21 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -3,15 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MoviesContextProvider from './Context/moviesContext';
+import ShowsContextProvider from './Context/tvContext';
+import SearchContextProvider from './Context/searchContext';
+import VideoContextProvider from './Context/videoContext';
+import UserContextProvider from './Context/userContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MoviesContextProvider>
+      <ShowsContextProvider>
+        <SearchContextProvider>
+          <VideoContextProvider>
+              <UserContextProvider>
+                  <App />
+              </UserContextProvider>
+          </VideoContextProvider>
+        </SearchContextProvider>
+      </ShowsContextProvider>
+    </MoviesContextProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
